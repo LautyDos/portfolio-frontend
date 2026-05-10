4e76479b2c593088f44423363d5ef9996c3dae0b
LautyDos <estudiandoyavanzando@gmail.com>
2026-05-10 15:27:17 -0300

    feat: add create-project feature (types and api call)

---
+++ src/features/create-project/api/createProjectApi.ts
@@ -0,0 +1,15 @@
+import type { ProjectDto } from "../../../entities/project/model/types";
+import type { CreateProjectRequest } from "../model/types";
+
+
+export async function createProjectApi(data: CreateProjectRequest): Promise<ProjectDto>{
+    const res = await fetch('/api/admin/projects', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+    })
+
+    if (!res.ok) throw new Error('Error al crear el proyecto')
+
+    return res.json()
+}
